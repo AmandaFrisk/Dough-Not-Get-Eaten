a13534f4f3b286e4f808c0e3c4e4d740376be9cf
AmandaFrisk <amanda.frisk33@gmail.com>
2022-07-14 20:58:21 -0600

    checkEaten added but timing is off

--- Dough-Not-Get-Eaten/js/app.js
+++ Dough-Not-Get-Eaten/js/app.js
@@ -1,8 +1,8 @@
 
 
 //DOM QUERYSELECTORS
-let sprinkles = document.querySelector("#doughnut")
-let hungry = document.querySelector('#mouth')
+let doughnut = document.querySelector("#doughnut")
+let mouth = document.querySelector('#mouth')
 //GLOBAL VARIABLES
 let bottomVar = 0
 let playerTurn = 0
@@ -15,8 +15,7 @@ let playerTurn = 0
 class Game {
 constructor(player1, player2){
     this.player1=player1
-    this.player2=player2
-    
+    this.player2=player2   
 }
 	    //  (name1 ?) (name2?)       
 //Methods :
@@ -33,25 +32,41 @@ constructor(player1, player2){
 //Add go btn
 //}
 
-
-
-
-
 jump(){
-    console.log("inside jump funct")
-    if(sprinkles.classList != ('animate')){   // only do the following if the classList hasn't added animate- This is so that each click doesn't add classList animate each time.
-sprinkles.classList.add('animate')   // access #doughnut via the sprinkles variable.Grab it's classList and use add() to animate- only runs one jump on it's own.
+ console.log("inside jump funct")
+    if(doughnut.classList != ('animate')){   // only do the following if the classList hasn't added animate- This is so that each click doesn't add classList animate each time.
+doughnut.classList.add('animate')   // access #doughnut via the sprinkles variable.Grab it's classList and use add() to animate- only runs one jump on it's own.
 setTimeout(function(){
-    sprinkles.classList.remove('animate');  //??? can't re add the same classList more than once. So have to remove after the animation is done.
+    doughnut.classList.remove('animate');  //??? can't re add the same classList more than once. So have to remove after the animation is done.
 } , 500)
+    }  
     
-let checkEaten =setInterval(function(){
-
-} ,10 ) ;
-
-
- }
+console.log("test")
 }
+ 
+
+
+
+checkEaten(){
+    console.log("inside check funct")
+    setInterval(function(){
+       const doughnutTop=parseInt(window.getComputedStyle(doughnut).getPropertyValue("top")); //get the top position of the #doughnut
+      // parsInt because without it it would return the top position with px but we only want the number.                                
+       const mouthLeft = parseInt(window.getComputedStyle(mouth).getPropertyValue("left")); //get the left position of the #mouth
+       if(mouthLeft===50 && doughnutTop>=160){ //   would be right under the doughnut && haven't jumped over entire mouth
+        
+        console.log("Doughnut was eaten")
+        mouth.style.animation ="none"; // turn off animation
+        mouth.style.display ="none"; // can't see it anymore
+       }
+   } ,10 ) ;  
+}
+
+}
+
+
+
+
 
 //goPlayer1(){
 //event listener onclick GO PLAYER 1 btn
@@ -133,7 +148,7 @@ let checkEaten =setInterval(function(){
 //Add tietime to tie statement
 //} 
 
-}
+
 let roundOne = new Game()
 //EVENT LISTENERS
-document.getElementById('jump-btn').addEventListener('click',(e)=>{roundOne.jump()}) 
+document.getElementById('jump-btn').addEventListener('click',(e)=>{roundOne.jump(),roundOne.checkEaten()}) 
